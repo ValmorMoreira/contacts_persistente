d20a4519923e04982b338e4124eec4afb73ec07c
ValmorMoreira <valfreestep@gmail.com>
2019-10-17 01:57:54 -0300

    Adicionado a listagem de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ContactsListPage } from '../pages/contacts-list/contacts-list';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -21,6 +23,7 @@ import { ContactsProvider } from '../providers/contacts/contacts';
     AboutPage,
     ContactPage,
     HomePage,
+    ContactsListPage,
     TabsPage
   ],
   imports: [
@@ -34,6 +37,7 @@ import { ContactsProvider } from '../providers/contacts/contacts';
     AboutPage,
     ContactPage,
     HomePage,
+    ContactsListPage,
     TabsPage
   ],
   providers: [
